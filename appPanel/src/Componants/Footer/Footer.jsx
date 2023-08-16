fb252c41fc28fb40169a4821987dbfa106e127ba
NicksEdits <61987116+NicksEdits@users.noreply.github.com>
2023-08-16 02:49:00 +0200

    Commit oublié
    oublié d'ajouter ce commit
    changement sur le footer (colonne pour le cote responsive )
     la navbar (hauteur de la nav)
    et indentation

--- appPanel/src/Componants/Footer/Footer.jsx
+++ appPanel/src/Componants/Footer/Footer.jsx
@@ -34,7 +34,7 @@ const Footer = () => {
             <div className='container'>
                 <div className='row'>
 
-                    <div className='col'>
+                    <div className='col-sm-12 col-md-8  '>
                         <div className='block md:flex '>
                             <div className='col'>
                                 <div className='footer-box'>
@@ -56,21 +56,21 @@ const Footer = () => {
                                 </div>
                             </div>
                             <div className='col'>
-                            <div className='footer-box'>
-                                <div className='row'>
+                                <div className='footer-box'>
+                                    <div className='row'>
 
-                                    <img src={pedago} loading="lazy" className="pb-2 w-16" />
-                                    <span className='title-peda'>
-                                        Ressources pédagogique
-                                    </span>
-                                    <p className='text-peda'>
-                                        Prochainement Loreum ipsum dolor sit amet Loreum ipsum dolor sit amet Loreum ipsum dolor sit amet Loreum ipsum dolor sit amet
-                                    </p>
+                                        <img src={pedago} loading="lazy" className="pb-2 w-16" />
+                                        <span className='title-peda'>
+                                            Ressources pédagogique
+                                        </span>
+                                        <p className='text-peda'>
+                                            Prochainement Loreum ipsum dolor sit amet Loreum ipsum dolor sit amet Loreum ipsum dolor sit amet Loreum ipsum dolor sit amet
+                                        </p>
 
+
+                                    </div>
 
                                 </div>
-
-                            </div>
                             </div>
                         </div>
                         <div className="  md:min-h-fit  left-[1000%]  hidden md:flex w-full  items-center ">
@@ -110,22 +110,22 @@ const Footer = () => {
                                 </div>
                             </div>
                             <div className="  md:min-h-fit  left-[1000%]  block md:hidden w-full  items-center ">
-                            <ul className=" text-[11px] text-white  pt-12 pb-12">
-                                <li>
-                                    <a href="/Acceuil" className='cursor-pointer flex '>
-                                        <img className="w-24 " src={logo2} alt="Logo Paris " title='' loading='lazy' />
-                                        <div className='separator'></div>
-                                        <img className="w-32 " src={logo1} alt=" Logo Parcours révolution" title='' loading='lazy' />
-                                    </a></li>
-                                {menu.map((menus) => (
-
+                                <ul className=" text-[11px] text-white  pt-12 pb-12">
                                     <li>
-                                        <a key={menus.name} id={menus.name} className="py-3" href={menus.link}>{menus.name}</a>
-                                    </li>
+                                        <a href="/Acceuil" className='cursor-pointer flex '>
+                                            <img className="w-24 " src={logo2} alt="Logo Paris " title='' loading='lazy' />
+                                            <div className='separator'></div>
+                                            <img className="w-32 " src={logo1} alt=" Logo Parcours révolution" title='' loading='lazy' />
+                                        </a></li>
+                                    {menu.map((menus) => (
 
-                                ))}
-                            </ul>
-                        </div>
+                                        <li>
+                                            <a key={menus.name} id={menus.name} className="py-3" href={menus.link}>{menus.name}</a>
+                                        </li>
+
+                                    ))}
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
